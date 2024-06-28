6e014895bc471905acb8b43c06d4e8bf5e7f6719
emoraisdev <evertonsmorais@gmail.com>
2024-06-28 01:05:10 -0300

    Implementando Segurança no Gateway

--- gateway/src/main/java/br/com/fiap/gateway/ConfigGateway.java
+++ gateway/src/main/java/br/com/fiap/gateway/ConfigGateway.java
@@ -3,22 +3,17 @@ package br.com.fiap.gateway;
 import br.com.fiap.gateway.routes.MsLoginRoutes;
 import br.com.fiap.gateway.security.AuthenticationFilter;
 import lombok.AllArgsConstructor;
-import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.Buildable;
-import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Map;
-import java.util.function.Function;
 
 @Configuration
 @AllArgsConstructor
 public class ConfigGateway {
 
     private final AuthenticationFilter authenticationFilter;
+
     private final MsLoginRoutes loginRoutes;
 
     @Bean
