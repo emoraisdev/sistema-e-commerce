5b83d0fad0a1f60b45dc2b6bf682d1104f11d26d
emoraisdev <evertonsmorais@gmail.com>
2024-07-22 22:17:24 -0300

    Removendo msprecos

--- msprecos/src/main/java/br/com/fiap/msprecos/exception/BusinessException.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.msprecos.exception;
-
-public class BusinessException extends RuntimeException{
-
-    public BusinessException(String mensagem) {
-        super(mensagem);
-    }
-}
--- msprecos/src/main/java/br/com/fiap/msprecos/exception/ParameterNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.msprecos.exception;
-
-public class ParameterNotFoundException extends RuntimeException{
-
-    public ParameterNotFoundException(String param) {
-        super("O Parâmetro %s é obrigatório.".formatted(param));
-    }
-}
